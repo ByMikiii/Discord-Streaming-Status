3a22960f01650d3595b6f5964c1b382b8479b4d2
ByMikiii <mikixr1@gmail.com>
2022-11-17 21:59:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Simple script that gives you the streaming status without actually streaming.
+Replace `"TOKEN"` with your discord token.
+Replace `"URL"` with any Twitch/Youtube address.
+You can change your streaming status by typing in any discord text channel `$stream text-that-you-want`.
+
+`pip install discord.ext.context==0.1.6` <br />
+`pip install discord.py==1.7.3`
+
+Shout-out to buxh ❤️
---
+++ main.py
@@ -0,0 +1,26 @@
+import discord, os
+from discord.ext import commands
+
+#Client
+bot = commands.Bot(
+    command_prefix = '$',
+    help_command=None,
+    self_bot=True
+)
+
+#Register Events
+@bot.event
+async def on_ready():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('logged in as {}'.format(bot.user))
+
+@bot.command()
+async def stream(ctx, *, message):
+    stream = discord.Streaming(
+        name=message,
+        url="URL"
+    )
+    await bot.change_presence(activity=stream)
+    await ctx.message.edit(content='Your streaming status: {}'.format(message))
+
+bot.run("TOKEN", bot=False, reconnect=True)
